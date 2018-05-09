7a8fa6cac680e34107f7145bb21f7cd560ba9a3a
John Chilton <jmchilton@gmail.com>
2018-05-09 16:02:03 -0400

    Fix up run_tests.sh formatting...

--- run_tests.sh
+++ run_tests.sh
@@ -17,13 +17,15 @@ fi
 
 . .venv/bin/activate
 planemo $PLANEMO_OPTIONS conda_init
-planemo $PLANEMO_OPTIONS profile_create --database_type "$PLANEMO_SERVE_DATABASE_TYPE" "$PLANEMO_PROFILE_NAME"
+planemo $PLANEMO_OPTIONS profile_create \
+        --database_type "$PLANEMO_SERVE_DATABASE_TYPE" \
+        "$PLANEMO_PROFILE_NAME"
 planemo $PLANEMO_OPTIONS serve \
         --daemon \
         --profile "$PLANEMO_PROFILE_NAME" \
         --port "$PLANEMO_SERVE_PORT" \
         "$1"
 planemo $PLANEMO_OPTIONS test \
-    --galaxy_url "$GALAXY_URL" \
-    --engine external_galaxy \
-    "$1"
+        --galaxy_url "$GALAXY_URL" \
+        --engine external_galaxy \
+        "$1"
