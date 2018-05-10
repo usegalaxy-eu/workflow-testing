3567f19a0e44ede85ae5be82254c37821dce496e
John Chilton <jmchilton@gmail.com>
2018-05-10 19:33:22 -0400

    Integrate docker-galaxy-stable into template.

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-: ${PLANEMO_TARGET:="planemo==0.49.2"}
+: ${PLANEMO_TARGET:="planemo==0.50.1"}
 : ${PLANEMO_OPTIONS:="--verbose"}
 : ${PLANEMO_PROFILE_NAME:="wxflowtest"}
 : ${PLANEMO_SERVE_PORT:="9019"}
-: ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}
+: ${SERVE_GALAXY:="native"}  # Set to docker to use dockerized Galaxy, native to use Galaxy in current enviornment.
+: ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}  # used if SERVE_GALAXY = native
+: ${PLANEMO_DOCKER_GALAXY_IMAGE:="quay.io/bgruening/galaxy:18.01"}  # used if SERVE_GALAXY = "docker"
 
 GALAXY_URL="http://localhost:$PLANEMO_SERVE_PORT"
 
-if [ ! -f .venv ]; then
+if [ ! -d .venv ]; then
     virtualenv .venv
     . .venv/bin/activate
     pip install -U pip
@@ -16,15 +18,27 @@ if [ ! -f .venv ]; then
 fi
 
 . .venv/bin/activate
-planemo $PLANEMO_OPTIONS conda_init
-planemo $PLANEMO_OPTIONS profile_create \
+if [ "$SERVE_GALAXY" = "native" ]; then
+    planemo $PLANEMO_OPTIONS conda_init
+    planemo $PLANEMO_OPTIONS profile_create \
         --database_type "$PLANEMO_SERVE_DATABASE_TYPE" \
         "$PLANEMO_PROFILE_NAME"
-planemo $PLANEMO_OPTIONS serve \
+    planemo $PLANEMO_OPTIONS serve \
         --daemon \
         --profile "$PLANEMO_PROFILE_NAME" \
         --port "$PLANEMO_SERVE_PORT" \
         "$1"
+else
+    docker pull "${PLANEMO_DOCKER_GALAXY_IMAGE}"
+    planemo $PLANEMO_OPTIONS serve \
+        --daemon \
+        --engine docker_galaxy \
+        --docker_galaxy_image "${PLANEMO_DOCKER_GALAXY_IMAGE}" \
+        --docker_extra_volume . \
+        --port "$PLANEMO_SERVE_PORT" \
+        "$1"
+fi
+
 planemo $PLANEMO_OPTIONS test \
         --galaxy_url "$GALAXY_URL" \
         --engine external_galaxy \
