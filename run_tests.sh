6dce0c99840c682d8021caed7206f3725259ed34
John Chilton <jmchilton@gmail.com>
2018-05-11 07:47:57 -0400

    Rearrange the script to make it clearer you can simplify it.
    Five different testing styles - started with some more complicated ones that might be useful for local development and debugging. For actual CI, probably not serving first and just issueing the test command is better.

--- run_tests.sh
+++ run_tests.sh
@@ -4,22 +4,26 @@
 : ${PLANEMO_OPTIONS:="--verbose"}
 : ${PLANEMO_PROFILE_NAME:="wxflowtest"}
 : ${PLANEMO_SERVE_PORT:="9019"}
-: ${SERVE_GALAXY:="native"}  # Set to docker to use dockerized Galaxy, native to use Galaxy in current enviornment.
-: ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}  # used if SERVE_GALAXY = native
-: ${PLANEMO_DOCKER_GALAXY_IMAGE:="quay.io/bgruening/galaxy:18.01"}  # used if SERVE_GALAXY = "docker"
+: ${PLANEMO_TEST_STYLE:="serve_and_test"}  # profile_serve_and_test, serve_and_test, docker_serve_and_test, test, docker_test
+: ${PLANEMO_SERVE_DATABASE_TYPE:="postgres"}  # used if not using Docker with PLANEMO_TEST_STYLE
+: ${PLANEMO_DOCKER_GALAXY_IMAGE:="quay.io/bgruening/galaxy:18.01"}  # used if used Docker with PLANEMO_TEST_STYLE
 
 GALAXY_URL="http://localhost:$PLANEMO_SERVE_PORT"
 
+# Ensure Planemo is installed.
 if [ ! -d .venv ]; then
     virtualenv .venv
     . .venv/bin/activate
     pip install -U pip
     pip install "${PLANEMO_TARGET}"
 fi
-
 . .venv/bin/activate
-if [ "$SERVE_GALAXY" = "native" ]; then
-    planemo $PLANEMO_OPTIONS conda_init
+
+# Run test.
+# This example shows off a bunch of different ways one could test with Planemo,
+# but for actual workflow testing projects - probably best just to take one of the last
+# two very easy invocations to simplify things.
+if [ "$PLANEMO_TEST_STYLE" = "profile_serve_and_test" ]; then
     planemo $PLANEMO_OPTIONS profile_create \
         --database_type "$PLANEMO_SERVE_DATABASE_TYPE" \
         "$PLANEMO_PROFILE_NAME"
@@ -28,7 +32,21 @@ if [ "$SERVE_GALAXY" = "native" ]; then
         --profile "$PLANEMO_PROFILE_NAME" \
         --port "$PLANEMO_SERVE_PORT" \
         "$1"
-else
+    planemo $PLANEMO_OPTIONS test \
+        --galaxy_url "$GALAXY_URL" \
+        --engine external_galaxy \
+        "$1"
+elif [ "$PLANEMO_TEST_STYLE" = "serve_and_test" ]; then
+    planemo $PLANEMO_OPTIONS serve \
+        --daemon \
+        --database_type "$PLANEMO_SERVE_DATABASE_TYPE" \
+        --port "$PLANEMO_SERVE_PORT" \
+        "$1"
+    planemo $PLANEMO_OPTIONS test \
+        --galaxy_url "$GALAXY_URL" \
+        --engine external_galaxy \
+        "$1"
+elif [ "$PLANEMO_TEST_STYLE" = "docker_serve_and_test" ]; then
     docker pull "${PLANEMO_DOCKER_GALAXY_IMAGE}"
     planemo $PLANEMO_OPTIONS serve \
         --daemon \
@@ -37,9 +55,22 @@ else
         --docker_extra_volume . \
         --port "$PLANEMO_SERVE_PORT" \
         "$1"
-fi
-
-planemo $PLANEMO_OPTIONS test \
+    planemo $PLANEMO_OPTIONS test \
         --galaxy_url "$GALAXY_URL" \
         --engine external_galaxy \
         "$1"
+elif [ "$PLANEMO_TEST_STYLE" = "test" ]; then
+    planemo $PLANEMO_OPTIONS test \
+        --database_type "$PLANEMO_SERVE_DATABASE_TYPE" \
+        "$1"
+elif [ "$PLANEMO_TEST_STYLE" = "docker_test" ]; then
+    docker pull "${PLANEMO_DOCKER_GALAXY_IMAGE}"
+    planemo $PLANEMO_OPTIONS test \
+        --engine docker_galaxy \
+        --docker_galaxy_image "${PLANEMO_DOCKER_GALAXY_IMAGE}" \
+        --docker_extra_volume . \
+        "$1"
+else
+    echo "Unknown test style ${PLANEMO_TEST_STYLE}"
+    exit 1
+fi
