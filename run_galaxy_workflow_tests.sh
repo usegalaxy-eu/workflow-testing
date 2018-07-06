9f9314d10ef75da0f8321a5b84093307937280ac
Helena Rasche <hxr@hx42.org>
2018-07-06 11:24:15 +0200

    cleanup more

--- run_galaxy_workflow_tests.sh
+++ run_galaxy_workflow_tests.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-set -ex
-
 : ${PLANEMO_OPTIONS:=""}  # e.g. PLANEMO_OPTIONS="--verbose"
-
-GALAXY_URL="https://usegalaxy.eu"
 
 # Run test.
 planemo  $PLANEMO_OPTIONS test \
-	--galaxy_url "$GALAXY_URL" \
+	--galaxy_url "https://usegalaxy.eu" \
 	--galaxy_user_key "$GALAXY_USER_KEY" --no_shed_install \
 	--engine external_galaxy \
 	"$1"
