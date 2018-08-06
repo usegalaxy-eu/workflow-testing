eed3baf40c36f02d3df5ae6c32ee3eb034377cd5
Helena Rasche <hxr@hx42.org>
2018-08-06 10:24:40 +0200

    support history_name in dev version of planemo

--- run_galaxy_workflow_tests.sh
+++ run_galaxy_workflow_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 : ${PLANEMO_OPTIONS:=""}  # e.g. PLANEMO_OPTIONS="--verbose"
 
+# Ensure we can find this history later.
+nonce=$(date +%s)
+history_name="$1 $nonce"
+
 # Run test.
 planemo  $PLANEMO_OPTIONS test \
+	--history_name "$history_name"
 	--galaxy_url "https://usegalaxy.eu" \
 	--galaxy_user_key "$GALAXY_USER_KEY" --no_shed_install \
 	--engine external_galaxy \
