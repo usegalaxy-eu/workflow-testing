02bcce81f3b974400222c1064cdc54e6be487cd6
Helena Rasche <hxr@hx42.org>
2018-08-06 14:54:46 +0200

    write out history.html instead

--- run_galaxy_workflow_tests.sh
+++ run_galaxy_workflow_tests.sh
@@ -18,5 +18,5 @@ set -e
 
 history_id=$(parsec histories get_histories --name "$history_name" | jq -r .[0].id)
 history_slug=$(parsec histories update_history --importable $history_id | jq -r .username_and_slug)
-echo "History published at https://usegalaxy.eu/$history_slug"
+echo "<html><head></head><body style=\"margin:0;padding:0;\"><iframe style=\"border:none;width:100%;height:100%;\" src=\"https://usegalaxy.eu/$history_slug\"></iframe></body></html>"> history.html
 exit $planemo_exit_code
