601c16eb20ca0aaac6926dddf37eb589ec0b7dcf
Helena Rasche <hxr@hx42.org>
2018-10-17 08:48:19 +0200

    fix training dir

--- update-training.sh
+++ update-training.sh
@@ -8,28 +8,31 @@ update_workflows() {
 
 	# For each -job.yml file, extract that workflows directory
 	for dir in $(find training-material/ -name '*-job.yml' | grep workflows | xargs dirname | sort -u); do
-		topic=$(echo "$x" | awk -F/ '{print $4}')
-		tutorial=$(echo "$x" | awk -F/ '{print $6}')
+		topic=$(echo "$dir" | awk -F/ '{print $3}')
+		tutorial=$(echo "$dir" | awk -F/ '{print $5}')
 
 		# into a nicer training/topic/tutorial/ dir
+		echo $dir $topic $tutorial
 		mkdir -p training/${topic}/;
 		cp -R $dir training/${topic}/${tutorial}/;
+	done
+}
+
+notify() {
+	echo "Please register or add the following:"
+	echo
+	find training -name '*-test.yml' | sed 's/-test.yml/.ga/'
+	echo
+	echo "And add the following to the readme:"
+	echo
+
+	for training in $(find training -name '*-test.yml' | sed 's/-test.yml/.ga/'); do
+		training_escape=$(echo $training | sed 's|/|%2F|g')
+		echo "$training |  [![Build Status](https://build.usegalaxy.eu/job/usegalaxy.eu/job/workflow-testing/PYTHON=System-CPython-2.7,WORKFLOW=${training_escape}/badge/icon)](https://build.usegalaxy.eu/job/usegalaxy.eu/job/workflow-testing/PYTHON=System-CPython-2.7,WORKFLOW=${training_escape}/)"
 	done
 }
 
 
 #clone
 update_workflows
-
-echo "Please register or add the following:"
-echo
-find training -name '*-test.yml' | sed 's/-test.yml/.ga/'
-echo
-echo "And add the following to the readme:"
-echo
-
-for training in $(find training -name '*-test.yml' | sed 's/-test.yml/.ga/'); do
-	training_escape=$(echo $training | sed 's|/|%2F|g')
-	echo "$training |  [![Build Status](https://build.usegalaxy.eu/job/usegalaxy.eu/job/workflow-testing/PYTHON=System-CPython-2.7,WORKFLOW=${training_escape}/badge/icon)](https://build.usegalaxy.eu/job/usegalaxy.eu/job/workflow-testing/PYTHON=System-CPython-2.7,WORKFLOW=${training_escape}/)"
-done
-
+notify
