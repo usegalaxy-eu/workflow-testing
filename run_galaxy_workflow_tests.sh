790d70213b14678f2292901eac46929a2ab7220c
Helena Rasche <hxr@hx42.org>
2018-11-05 15:55:02 +0100

    cleanup on success

--- run_galaxy_workflow_tests.sh
+++ run_galaxy_workflow_tests.sh
@@ -17,7 +17,14 @@ planemo $PLANEMO_OPTIONS test \
 planemo_exit_code=$?
 set -e
 
-history_id=$(parsec histories get_histories --name "$history_name" | jq -r .[0].id)
-history_slug=$(parsec histories update_history --importable $history_id | jq -r .username_and_slug)
-echo "<html><head></head><body style=\"margin:0;padding:0;\"><iframe style=\"border:none;width:100%;height:100%;\" src=\"https://usegalaxy.eu/$history_slug\"></iframe></body></html>"> history.html
+if (( planemo_exit_code > 0 )); then
+	history_id=$(parsec histories get_histories --name "$history_name" | jq -r .[0].id)
+	history_slug=$(parsec histories update_history --importable $history_id | jq -r .username_and_slug)
+	echo "<html><head></head><body style=\"margin:0;padding:0;\"><iframe style=\"border:none;width:100%;height:100%;\" src=\"https://usegalaxy.eu/$history_slug\"></iframe></body></html>"> history.html
+else
+	# Otherwise immediately delete
+	history_id=$(parsec histories get_histories --name "$history_name" | jq -r .[0].id)
+	parsec histories delete_history --purge $history_id
+	echo "<html><head></head><body>Test was completely successful</body></html>"> history.html
+fi
 exit $planemo_exit_code
