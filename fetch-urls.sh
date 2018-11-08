a62843f8a5a05fe34d3e54960aba5b8957d3b484
Helena Rasche <hxr@hx42.org>
2018-11-08 10:30:22 +0100

    remove urls.txt

--- fetch-urls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIRECTORIES=$(find . -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-for dir in $DIRECTORIES; do
-	if [[ -f "$dir/urls.txt" ]]; then
-		bash -c "cd $dir && wget --no-clobber --continue -i urls.txt"
-	fi
-done
-
